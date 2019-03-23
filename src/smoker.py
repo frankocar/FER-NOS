5f6ef12f88444224398483aaddd3330cb6f877da
Franko <frankocar@gmail.com>
2019-03-23 18:49:38 +0100

    Additional access control

--- src/smoker.py
+++ src/smoker.py
@@ -8,17 +8,17 @@ items = ["paper", "tobacco", 'matches']
 
 def smoker(worker_queue, merchant_queue, type):
     while True:
-        item, _ = worker_queue.receive(type=items.index(type) + 1)
-        print(f"    Smoker with {type}: received {item.decode('utf-8')}")
+        type_index = items.index(type) + 1
+        item, _ = worker_queue.receive(type=type_index)
+        # print(f"    Smoker with {type}: offered {item.decode('utf-8')}")
+        merchant_queue.send(message=str(type_index), type=4)
+        msg, recv_index = worker_queue.receive(type=type_index)
+        print(f"    Merchant sells to smoker with {items[recv_index - 1]}")
         time.sleep(1)
-        merchant_queue.send(message=f"Smoker with {type}: smoked", type=4)
-
-
-workers = []
+        merchant_queue.send(message=str(type_index), type=4)
 
 
 def main():
-    global workers
     worker_queue = sysv_ipc.MessageQueue(None, sysv_ipc.IPC_CREAT | sysv_ipc.IPC_EXCL)
     merchant_queue = sysv_ipc.MessageQueue(None, sysv_ipc.IPC_CREAT | sysv_ipc.IPC_EXCL)
     print("Starting...")
@@ -29,15 +29,18 @@ def main():
     while True:
         smp = random.sample(items, len(items) - 1)
         type = [i for i, x in enumerate(items) if x not in smp][0] + 1
-        print(f'Merchant sells {smp}')
+        print(f'    Merchant sells {smp}')
         worker_queue.send(message=str(smp), type=type)
-        print('    ' + merchant_queue.receive(type=4)[0].decode('utf-8'))
+        msg = merchant_queue.receive(type=4)[0].decode('utf-8')
+        print(f"    Smoker with {items[int(msg) - 1]}: request")
+        smoker_type = int(msg[-1])
+        worker_queue.send(message="", type=smoker_type)
+        recv_type = int(merchant_queue.receive(type=4)[0].decode('utf-8'))
+        print(f"    Smoker with {items[recv_type - 1]}: 🚬")
+
+    # for w in workers:
+    #     w.join()
 
 
 if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        for w in workers:
-            w.join()
-        print("Out")
+    main()
