00d08f8b7317238b843c828437a16e9457c605e4
Franko <frankocar@gmail.com>
2019-03-24 11:55:11 +0100

    Added chopsticks

--- src/philosophers.py
+++ src/philosophers.py
@@ -1,10 +1,16 @@
-from multiprocessing import Process, Pipe
+from multiprocessing import Process, Pipe, Array
 import queue
 import time
 import random
 import enum
+import os
 
+detail_output = True
+long_sleep = False
 philosopher_number = 5
+
+# chopsticks = [False for i in range(philosopher_number)]
+chopsticks = Array('b', [False for i in range(philosopher_number)])
 
 
 class MessageType(enum.Enum):
@@ -34,12 +40,16 @@ def message_from_string(string):
     return Message(int(split[0]), int(split[1]), MessageType(split[2]))
 
 
+def dprint(txt):
+    if detail_output:
+        print(txt)
+
+
 class Philosopher(Process):
     def __init__(self, id):
         super(Philosopher, self).__init__()
         self.id = id
-        self.left_fork = False
-        self.right_fork = False
+        self.chopsticks = False
         self.q = queue.PriorityQueue()
         self.in_pipes = {}
         self.out_pipes = {}
@@ -59,17 +69,36 @@ class Philosopher(Process):
     def add_request(self, request):
         self.q.put((request.clock, request))
 
+    def aquire_chopsticks(self):
+        if not chopsticks[self.id - 1] and not chopsticks[self.id]:
+            # print(f"ID:{self.id} - {[x for x in chopsticks]}")
+            chopsticks[self.id - 1] = True
+            chopsticks[self.id] = True
+            self.chopsticks = True
+            # print(f"set -- ID:{self.id} - {[x for x in chopsticks]}")
+            return True
+        return False
+
+    def release_chopsticks(self):
+        chopsticks[self.id - 1] = False
+        chopsticks[self.id] = False
+        self.chopsticks = False
+        return True
+
     def run(self):
-        print(f"Started philosopher {self.id}")
+        print(f"Started philosopher {self.id} - {os.getpid()}")
         responses = 0
         wait = False
 
         init_time = self.clock
 
         while True:
-            time.sleep(2)
+            time.sleep(1 if long_sleep else 0.1)
             if not wait and random.choice([True, False]):
-                print(f"Philosopher {self.id} wants to eat")
+                if not self.chopsticks:
+                    dprint(f"Philosopher {self.id} wants to eat")
+                else:
+                    dprint(f"Philosopher {self.id} wants to leave")
                 request = Message(self.id, self.clock, MessageType.request)
                 init_time = self.clock
                 self.send_requests(str(request))
@@ -88,33 +117,32 @@ class Philosopher(Process):
                         self.out_pipes[recv.id].send(str(Message(self.id, self.clock, MessageType.response)))
                     elif recv.type == MessageType.response:
                         responses += 1
-                        print(f"Philosopher {self.id} received a response from {recv.id}")
+                        dprint(f"Philosopher {self.id} received a response from {recv.id}")
                     elif recv.type == MessageType.exit:
                         # TODO
                         tmp = self.q.get()[1]
                         if tmp.id != recv.id and tmp.clock != recv.clock:
                             print('Error removing from queue')
-                        print(f"Philosopher {self.id} removed a message from {recv.id}")
-                        if wait:
-                            # TODO
-                            pass
+                            self.add_request(tmp)
+                        dprint(f"Philosopher {self.id} removed a message from {recv.id}")
 
             if self.q.empty():
                 continue
 
             tmp = self.q.get()[1]
             if tmp.id == self.id and responses >= (philosopher_number - 1):
-                print(f"Philosopher {self.id} sits")
-                time.sleep(2)
+                time.sleep(1 if long_sleep else 0.1)
+                if not self.chopsticks:
+                    if self.aquire_chopsticks():
+                        print(f"\033[0;32;0mPhilosopher {self.id} sits\033[0;0;0m")
+                else:
+                    if self.release_chopsticks():
+                        print(f"\033[0;31;0mPhilosopher {self.id} leaves\033[0;0;0m")
                 responses = 0
                 wait = False
-                print(f"Philosopher {self.id} leaves")
                 self.send_requests(Message(self.id, init_time, MessageType.exit))
             else:
                 self.add_request(tmp)
-
-
-
 
 
 def main():
@@ -125,6 +153,7 @@ def main():
             if x is p:
                 continue
             a, b = Pipe(False)  # disable duplex comm
+
             p.add_out_pipe(x.id, b)
             x.add_in_pipe(p.id, a)
 
